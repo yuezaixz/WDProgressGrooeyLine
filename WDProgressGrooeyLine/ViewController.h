9cef97de2414d300d096676dd8d4ff7a48ae79ae
yuezaixz <303178394@qq.com>
2016-02-18 19:36:42 +0800

    Initial Commit

---
+++ WDProgressGrooeyLine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WDProgressGrooeyLine
+//
+//  Created by 吴迪玮 on 16/2/18.
+//  Copyright © 2016年 DNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WDProgressGrooeyLine/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WDProgressGrooeyLine
+//
+//  Created by 吴迪玮 on 16/2/18.
+//  Copyright © 2016年 DNT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
